59417da1c6837d4c9c55309b22a3bcff1d2b0d6e
sannaga3 <n.tetsuya02@gmail.com>
2021-03-10 11:41:33 +0900

    ユーザーモデルにバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { in: 1..20 }
+  validates :email, presence: true, length: { in: 1..50 }, 
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
