2cb2cadc3cdf5ccb93265a15cf24c142b5e335e4
sannaga3 <n.tetsuya02@gmail.com>
2021-03-10 12:43:15 +0900

    emailカラムにインデックス追加し、一意性を付与

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
+  before_validation { email.downcase! }
   validates :name, presence: true, length: { in: 1..20 }
   validates :email, presence: true, length: { in: 1..50 }, 
-                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+  validates :email, uniqueness: true
 end
