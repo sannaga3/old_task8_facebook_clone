1f850cc23741ca36c0dff57fb5d0a037da6b6f27
sannaga3 <n.tetsuya02@gmail.com>
2021-03-10 14:41:51 +0900

    confirmアクション及び登録内容確認ページ作成、ストロングパラメータ設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,20 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def confirm
+    @user = User.new(user_params)
+    @user.id = params[:id]
+    render :new if @user.invalid?
+  end
+  
   def show
   end
 
   def edit
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password,:password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :show, :edit, :confirm, :destroy]
+  resources :users, only: [:new, :show, :edit, :confirm, :destroy] do
+    collection do
+      post :confirm
+      patch :confirm
+    end
+    member do
+      patch :confirm
+    end
+  end
 end
