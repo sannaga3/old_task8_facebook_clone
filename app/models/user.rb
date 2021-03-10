a2f24e4ecaae930743e7776a679d6403d85c4c7b
sannaga3 <n.tetsuya02@gmail.com>
2021-03-10 13:03:39 +0900

    セキュアパスワード設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   before_validation { email.downcase! }
   validates :name, presence: true, length: { in: 1..20 }
   validates :email, presence: true, length: { in: 1..50 }, 
-                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :email, uniqueness: true
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
