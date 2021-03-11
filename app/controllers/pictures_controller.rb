6338726f3a1f2f6c131c1cb82f77be26a19b3c43
sannaga3 <n.tetsuya02@gmail.com>
2021-03-11 16:57:31 +0900

    ピクチャー投稿ページ作成

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,8 +1,14 @@
 class PicturesController < ApplicationController
   def index
+    @picture = Picture.all
   end
 
   def new
+    if params[:back]
+      @picture = Feed.new(feed_params)
+    else
+      @picture = Feed.new
+    end
   end
 
   def create
