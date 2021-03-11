383fc00996a8f05bc800dc55edf29d09c202e65a
sannaga3 <n.tetsuya02@gmail.com>
2021-03-11 13:45:26 +0900

    ログインページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show, :edit, :update, :destroy] do
     collection do
       post :confirm
