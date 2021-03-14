f1f812e34e7e9946b2933bc4bc0f093f58f358f0
sannaga3 <n.tetsuya02@gmail.com>
2021-03-14 17:27:23 +0900

    投稿一覧ページのcss作成

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   storage :file
-  process :resize_to_limit => [480, 320]
+  process :resize_to_limit => [460, 306]
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
